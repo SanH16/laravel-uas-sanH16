d776b305c12153a65dc3d4dc474b5e65898705db
Akbar Dwi <akbardwi500@gmail.com>
2022-07-03 00:48:24 +0700

    1

---
+++ warungsmoke/app/Http/Controllers/ProdiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\Prodi;
+
+class ProdiController extends Controller
+
+{
+    public function index()
+    {
+        
+    }
+}
